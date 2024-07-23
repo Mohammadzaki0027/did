33b37fb0564f0823af0860dcf61f221cdab12407
Zaki Shaikh <zakishaikh@Zakis-MacBook-Pro.local>
2024-07-23 18:24:23 +0530

    script added

--- models/identity.js
+++ models/identity.js
@@ -1,4 +1,4 @@
-// models/identity.js
+
 const mongoose = require('mongoose');
 const { v4: uuidv4 } = require('uuid');
 
@@ -6,7 +6,8 @@ const identitySchema = new mongoose.Schema({
     vehicleNumber: { type: String, required: true, unique: true },
     did: { type: String, required: true },
     privateKey: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now }
+    createdAt: { type: Date, default: Date.now },
+    
 });
 
 module.exports = mongoose.model('Identity', identitySchema);
